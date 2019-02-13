6187e3c03c5180419ac04c1a0dce766bf0248666
margaritakop <margarita.kopniczky@gmail.com>
2019-02-13 13:06:59 +0000

    add scaffold for binary search code

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,12 +1,17 @@
 public class BinarySearch {
-    public static int[] time (int[] arraySorted, int searchNumber){
-        //define the data a top
-        //use some alg to sort the array - we assume a sorted array
-        int[] result = {10,20,30,40,50};
-        //loops
-        //edge case where there are multiple entries to te same  check if its the same as before
-        //not to be stuck in a loop
+    public static long searchTime (int[] arraySorted, int searchNumber){
+        //result returned is the time that it took to fin the searchNumber in the arraySorted.
 
-        return result;
+        //set timer
+        long startTime = System.currentTimeMillis();
+
+        boolean isSearchNumberFound=false;
+        //carry out binary search
+        if(isSearchNumberFound == true){
+                long timeElapsed = System.currentTimeMillis() - startTime;
+                return timeElapsed;
+        }
+        //TODO: handle edge case better when the searchNumber is not found
+        return 404; //the number 404 is returned when the Searchnumber is not found
     }
 }

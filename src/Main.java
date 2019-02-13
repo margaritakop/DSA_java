88ab22ac639ed39d0383a31dad8dbbf6a1b380c2
margaritakop <margarita.kopniczky@gmail.com>
2019-02-13 14:20:40 +0000

    refactor Main search

--- src/Main.java
+++ src/Main.java
@@ -8,21 +8,23 @@ public class Main {
 
     public static void main(String[] args) {
 
-
-        ArrayList<Integer> randomList =generateRandomArray(100);
-        int[] array = randomList.stream().mapToInt(i -> i).toArray();
+        //make a random array of given length
+        int arrayLength = 100;
+        int[] array = generateRandomArray(arrayLength);
+        System.out.println("Sorted random array from 1 to " + arrayLength + " :");
         System.out.println(Arrays.toString(array));
 
         //select a number randomly for searchNumber
         int searchNumber = getRandom(array);
-        System.out.println(searchNumber);
+        System.out.println("Searching for: " + searchNumber);
 
-        System.out.println(LinearSearch.searchTime(array, searchNumber));
+        //search for the target searchnumber in the array
+        long searchTime = LinearSearch.searchTime(array, searchNumber);
+        System.out.println("Time of search was " + searchTime + " ms");
 
-        //System.out.println(Arrays.toString(BinarySearch.time(array, 3)));
     }
 
-    public static ArrayList<Integer> generateRandomArray(int n){
+    public static int[] generateRandomArray(int n){
         ArrayList<Integer> list = new ArrayList<Integer>(n);
         Random random = new Random();
 
@@ -32,7 +34,9 @@ public class Main {
             Collections.sort(list);
         }
 
-        return list;
+        int[] array = list.stream().mapToInt(i -> i).toArray();
+
+        return array;
     }
 
     public static int getRandom(int[] array) {

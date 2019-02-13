4190bc4d096c3703837b3f510d7f7fe57cb79585
margaritakop <margarita.kopniczky@gmail.com>
2019-02-13 12:55:39 +0000

    refactor linear search

--- src/LinearSearch.java
+++ src/LinearSearch.java
@@ -3,29 +3,17 @@ import java.util.concurrent.TimeUnit;
 public class LinearSearch {
     public static long searchTime (int[] arraySorted, int searchNumber){
         //result returned is the time that it took to fin the searchNumber in the arraySorted.
-        //if not fount it should return NaN
 
-        //iterate through the array and chack if elements equal the search Number
-        int time = 0; //set timer to 0
-
-
-// Program to measure elapsed time in Java
-
-                long startTime = System.currentTimeMillis();
-
-                /* ... the code being measured starts ... */
-                for (int num : arraySorted){
-                    if (num == searchNumber){
-                        //return time;
-                        long endTime = System.currentTimeMillis();
-                        long timeElapsed = endTime - startTime;
-                        System.out.println("Execution time in milliseconds: " + timeElapsed);
-                        return timeElapsed;
-                    }
-                }
-
-        return 404; //should return something meaningful if searchNumber is not in the array
+        //set timer
+        long startTime = System.currentTimeMillis();
+        //iterate through the array and check if elements equal the search Number
+        for (int num : arraySorted){
+            if (num == searchNumber){
+                long timeElapsed = System.currentTimeMillis() - startTime;
+                return timeElapsed;
+            }
+        }
+        //TODO: handle edge case better when the searchNumber is not found
+        return 404; //the number 404 is returned when the Searchnumber is not found
     }
-
-
 }
